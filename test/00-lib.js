a328a2d5d9f35eabc70f05e4804bc124c2e13498
Ben Zörb <ben@sommerlaune.com>
2017-07-03 06:17:48 +0200

    Ignore print styles (#113)
    Remove empty css rules
    
    Appveyor tweaks
    
    It seems appveyor is using some outdated packages from cache
    
    bump inline-critical
    
    fixed tests

--- test/00-lib.js
+++ test/00-lib.js
@@ -4,17 +4,19 @@ const path = require('path');
 const assert = require('chai').assert;
 const File = require('vinyl');
 const file = require('../lib/file-helper');
+const core = require('../lib/core');
+const read = require('./helper/testhelper').read;
 
 // Unittests
 describe('Lib', () => {
     describe('file.guessBasePath', () => {
-        it('should return process.cwd() without base', done => {
+        it('return process.cwd() without base', done => {
             const res = file.guessBasePath({});
             assert.strictEqual(res, process.cwd());
             done();
         });
 
-        it('should return base if base option is set', done => {
+        it('return base if base option is set', done => {
             const res = file.guessBasePath({src: 'fixtures/folder/generate-default.html'});
             assert.strictEqual(res, 'fixtures/folder');
             done();
@@ -29,7 +31,7 @@ describe('Lib', () => {
             });
         }
 
-        it('should compute path based on file', done => {
+        it('compute path based on file', done => {
             const f = file.replaceAssetPaths(html(), {
                 base: 'fixtures'
             });
@@ -49,7 +51,7 @@ describe('Lib', () => {
             done();
         });
 
-        it('should compute path based on dest', done => {
+        it('compute path based on dest', done => {
             const f = file.replaceAssetPaths(html(), {
                 base: 'fixtures',
                 dest: 'fixtures/1/2/3.html'
@@ -70,7 +72,7 @@ describe('Lib', () => {
             done();
         });
 
-        it('should compute path based on destFolder', done => {
+        it('compute path based on destFolder', done => {
             const f = file.replaceAssetPaths(html(), {
                 base: 'fixtures',
                 dest: '1/2/3.html',
@@ -92,7 +94,7 @@ describe('Lib', () => {
             done();
         });
 
-        it('should compute path based on dest with src outside base', done => {
+        it('compute path based on dest with src outside base', done => {
             const f = file.replaceAssetPaths(html('fixtures', path.resolve('expexted/generate-default.html')), {
                 base: 'fixtures',
                 dest: 'fixtures/1/2.html'
@@ -111,6 +113,34 @@ describe('Lib', () => {
             assert.strictEqual(f(mock('images/test.png')).contents.toString(), 'url(../a/images/test.png)');
 
             done();
+        });
+    });
+
+    describe('core.appendStylesheets', () => {
+        it('ignore print styles', () => {
+            const func = core.appendStylesheets({base: path.join(__dirname, '..')});
+            const htmlfile = new File({
+                path: 'fixtures/print.html',
+                contents: Buffer.from(read('fixtures/print.html'))
+            });
+            return func(htmlfile).then((htmlfile => {
+                const stylesheets = htmlfile.stylesheets.map(p => file.normalizePath(p));
+                assert.include(stylesheets, 'fixtures/styles/include.css');
+                assert.notInclude(stylesheets, 'fixtures/styles/print.css');
+            }));
+        });
+
+        it('ignore print styles from rel="preload"', () => {
+            const func = core.appendStylesheets({base: path.join(__dirname, '..')});
+            const htmlfile = new File({
+                path: 'fixtures/print.html',
+                contents: Buffer.from(read('fixtures/preload.html'))
+            });
+            return func(htmlfile).then((htmlfile => {
+                const stylesheets = htmlfile.stylesheets.map(p => file.normalizePath(p));
+                assert.include(stylesheets, 'fixtures/styles/include.css');
+                assert.notInclude(stylesheets, 'fixtures/styles/print.css');
+            }));
         });
     });
 });
